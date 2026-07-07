706c5721cf30cab6462a43cfb56d3ec1a8942eb6
ridhima <ridhimavijay2003@gmail.com>
2026-07-07 18:35:44 +0530

    add

--- flower-shop-react/src/App.jsx
+++ flower-shop-react/src/App.jsx
@@ -1,1 +1,12 @@
+function App() {
+    return (
+        <div>
+            <div className="container mt-4">
+                <h1>Welcome to Flower Shop!</h1>
+            </div>
+        </div>
+    );
+}
+
+export default App;
 
